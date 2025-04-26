f3cbbc45a4205870c6eee2bab49b191798e21d28
navin patidar <navin.patidar@gmail.com>
2025-04-26 11:42:30 -0700

    [nvim] refactor TmuxNavigater mappings

--- configs/neovim/nvim/lua/mappings.lua
+++ configs/neovim/nvim/lua/mappings.lua
@@ -1,17 +1,17 @@
 require "nvchad.mappings"
 
 -- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
-vim.keymap.set('n', 'C-h', '<cmd><C-U>TmuxNavigateLeft<CR>')
-vim.keymap.set('n', 'C-l', '<cmd><C-U>TmuxNavigateRight<CR>')
-vim.keymap.set('n', 'C-j', '<cmd><C-U>TmuxNavigateDown<CR>')
-vim.keymap.set('n', 'C-k', '<cmd><C-U>TmuxNavigateUp<CR>')
+map('n', 'C-h', '<cmd><C-U>TmuxNavigateLeft<CR>')
+map('n', 'C-l', '<cmd><C-U>TmuxNavigateRight<CR>')
+map('n', 'C-j', '<cmd><C-U>TmuxNavigateDown<CR>')
+map('n', 'C-k', '<cmd><C-U>TmuxNavigateUp<CR>')
+
 
 -- Window navigation key maps
 --map('n', '<c-k>', ':wincmd k<CR>')
