3154e6094881fe791a96e88daa8668415e374c02
navin patidar <navin.patidar@gmail.com>
2025-05-02 21:51:53 -0700

    [ripgrep] Add fzf bindings
    ctrl+e : open file with nvim editor
    ctrl+q : open shell into file dir

---
+++ configs/ripgrep/rg.rc.sh
@@ -0,0 +1,22 @@
+# ripgrep->fzf->vim [QUERY]
+r_grep() (
+  RELOAD='reload:rg --column --color=always --smart-case {q} || :'
+  OPENER='if [[ $FZF_SELECT_COUNT -eq 0 ]]; then
+           nvim {1} +{2}     # No selection. Open the current line in Vim.
+          else
+           nvim +cw -q {+f}  # Build quickfix list for the selected items.
+          fi'
+  fzf --disabled --ansi --multi \
+    --bind "start:$RELOAD" --bind "change:$RELOAD" \
+    --bind "enter:become:$OPENER" \
+    --bind "ctrl-e:execute:$OPENER" \
+    --bind 'ctrl-q:execute:(cd $(dirname {1}) && zsh)' \
+    --bind 'alt-a:select-all,alt-d:deselect-all,ctrl-/:toggle-preview' \
+    --delimiter : \
+    --preview 'bat --style=full --color=always --highlight-line {2} {1}' \
+    --preview-window '~4,+{2}+4/3,<80(up)' \
+    --query "$*"
+)
+
+zle -N r_grep
+bindkey '^w' r_grep
