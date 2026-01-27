2ac0ea69744dec16d7c25ef618befaf315528184
navin patidar <navin.patidar@gmail.com>
2026-01-26 18:11:25 -0800

    [nvim] config fzf-lua to load on start-up
    TSIA

--- configs/nvim/lua/plugins/fzf.lua
+++ configs/nvim/lua/plugins/fzf.lua
@@ -1,9 +1,10 @@
 return {
   "ibhagwan/fzf-lua",
+  lazy = false,
   -- optional for icon support
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     -- calling `setup` is optional for customization
-    require("fzf-lua").setup({})
+    require("fzf-lua").setup {}
   end,
 }
