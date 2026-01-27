4056167ac57b183eb49586e4d412e4d1afa4525d
navin patidar <navin.patidar@gmail.com>
2026-01-26 18:03:17 -0800

    [nvim] add workspace plugin
    TSIA

---
+++ configs/nvim/lua/plugins/workspaces.lua
@@ -0,0 +1,12 @@
+return {
+  "natecraddock/workspaces.nvim",
+  lazy = false,
+  config = function()
+    require("workspaces").setup ({
+      -- Optional: Add configuration options here, e.g., hooks
+      hooks = {
+        open = { "Telescope find_files" },
+      },
+    })
+  end,
+}
