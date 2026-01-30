735f0bdf81fa6ffb8154dfc862b59908ab7db1d8
navin patidar <navin.patidar@gmail.com>
2026-01-29 20:35:27 -0800

    [lazyVim] keymaps for termial
    TSIA

--- configs/lazyVim/lua/config/keymaps.lua
+++ configs/lazyVim/lua/config/keymaps.lua
@@ -2,9 +2,26 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local map = vim.keymap.set
+vim.keymap.setmap("n", ";", ":", { desc = "CMD enter command mode" })
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
+-- Normal mode
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Switch to left pane" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Switch to bottom pane" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Switch to top pane" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Switch to right pane" })
 
--- Use ctrl+ESC to switch to normal mode from terminal mode in terminal
-vim.api.nvim_set_keymap("t", "<C-n>", "<C-\\><C-n>", { noremap = true, silent = true })
+-- terminal mode
+vim.keymap.set("t", "<C-h>", "<C-\\><C-n><C-w>h", { desc = "Switch to left pane" })
+vim.keymap.set("t", "<C-j>", "<C-\\><C-n><C-w>j", { desc = "Switch to bottom pane" })
+vim.keymap.set("t", "<C-k>", "<C-\\><C-n><C-w>k", { desc = "Switch to top pane" })
+vim.keymap.set("t", "<C-l>", "<C-\\><C-n><C-w>l", { desc = "Switch to right pane" })
+
+-- Use jk keys to switch to normal mode from terminal mode in terminal
+vim.api.nvim_set_keymap("t", "jk", "<C-\\><C-n>", { noremap = true, silent = true })
+-- Map <Leader>bt to open a terminal in a new tab
+vim.api.nvim_set_keymap("n", "<leader>bt", ":tabnew | :terminal<CR>", { noremap = true, silent = true })
+-- Map <leader>th to open a terminal in horizontal orientation
+vim.keymap.set("n", "<leader>th", ":split | terminal<cr>", { noremap = true, silent = true })
+-- Map <leader>tv to open a terminal in vertical orientation
+vim.keymap.set("n", "<leader>tv", ":vsplit | term<CR>")
+
