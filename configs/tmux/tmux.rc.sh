43d71fb87a52fa3ad680ce5a434ca4a7b2d79cd7
navin patidar <navin.patidar@gmail.com>
2026-02-03 23:03:27 -0800

    [tmux] Add tmux related alias

--- configs/tmux/tmux.rc.sh
+++ configs/tmux/tmux.rc.sh
@@ -1,2 +1,11 @@
-alias tmux="tmux -f ${XDG_CONFIG_HOME}/tmux/tmux.conf"
+alias t="tmux"
+alias jk="tmux copy-mode"
+alias ta="tmux attach"
 
+alias ts="tmux list-windows -F \#S:#W,#I | \
+ fzf \
+     --delimiter="," \
+     --preview="echo {0} {1}" \
+"
+# --priview="tmux capture-pane -p -e -t {} | bat " \
+#//--bind="enter:become"\
