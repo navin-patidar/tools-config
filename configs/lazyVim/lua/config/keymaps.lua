285f36f19a10000007d7fe30d7b039960b855f5f
navin patidar <navin.patidar@gmail.com>
2026-02-03 22:17:14 -0800

    [nvim] Add exec() fuction to execute the commands in floating window
    TSIA

--- configs/lazyVim/lua/config/keymaps.lua
+++ configs/lazyVim/lua/config/keymaps.lua
@@ -5,6 +5,7 @@
 vim.keymap.set("n", ";", ":", { desc = "CMD enter command mode" })
 
 vim.keymap.set({ "n", "v" }, "<leader>uet", require("stay-centered").toggle, { desc = "Toggle stay-centered.nvim" })
+
 -- Normal mode
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Switch to left pane" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Switch to bottom pane" })
@@ -25,3 +26,41 @@ vim.api.nvim_set_keymap("n", "<leader>bt", ":tabnew | :terminal<CR>", { noremap 
 vim.keymap.set("n", "<leader>th", ":split | terminal<cr>", { noremap = true, silent = true })
 -- Map <leader>tv to open a terminal in vertical orientation
 vim.keymap.set("n", "<leader>tv", ":vsplit | term<CR>")
+
+function exec(cmd, dir, direction, name, go_back)
+  require("toggleterm").exec(cmd .. " && exit", 1, 12, dir, direction, name, go_back, true)
+end
+
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>ts",
+  "<cmd> lua exec('lazygit', nil, 'float', 'TMUX Teminals', true) <CR>",
+  { desc = "Switch to TMUX terminal", noremap = true, silent = true }
+)
+
+-- function terminal_toggle(cmd)
+--   local Terminal = require("toggleterm.terminal").Terminal
+--   local tmp = Terminal:new({
+--     cmd = cmd,
+--     dir = dir or nil,
+--     direction = direction or "float",
+--     float_opts = {
+--       border = "double",
+--     },
+--     -- function to run on opening the terminal
+--     on_open = function(term)
+--       vim.cmd("startinsert!")
+--       vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+--     end,
+--     -- function to run on closing the terminal
+--     on_close = function(term)
+--       vim.cmd("startinsert!")
+--     end,
+--   })
+--
+--   tmp.cmd = cmd
+--   tmp:toggle()
+-- end
+--
+-- vim.api.nvim_set_keymap("n", "<leader>tx", "<cmd>lua terminal_toggle('fzf')<CR>", { noremap = true, silent = true })
+--
