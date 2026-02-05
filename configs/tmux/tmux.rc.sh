3279539e08444b6f926e54649089bbcbac2d1a03
navin patidar <navin.patidar@gmail.com>
2026-02-04 20:21:38 -0800

    [tmux] Add "ts" alias to swith between tmux panes

--- configs/tmux/tmux.rc.sh
+++ configs/tmux/tmux.rc.sh
@@ -2,10 +2,7 @@ alias t="tmux"
 alias jk="tmux copy-mode"
 alias ta="tmux attach"
 
-alias ts="tmux list-windows -F \#S:#W,#I | \
- fzf \
-     --delimiter="," \
-     --preview="echo {0} {1}" \
-"
-# --priview="tmux capture-pane -p -e -t {} | bat " \
-#//--bind="enter:become"\
+LIST_WINDOWS="tmux list-panes -aF '#{session_name}:#{window_index} #{window_index} #{window_name}' |  grep -v \"${TMUX_PANE}\""
+PREVIEW_PANE="tmux capture-pane -p -e -t {1} | bat --color=always"
+
+alias ts="${LIST_WINDOWS} |  fzf --delimiter=' ' --preview=\"${PREVIEW_PANE}\" "
