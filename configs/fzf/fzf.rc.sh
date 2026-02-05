8cd8a203701cbdd88c7ea354b4562c8e694c535c
navin patidar <navin.patidar@gmail.com>
2026-02-04 17:59:04 -0800

    [fzf] show fzf input bar at the top

--- configs/fzf/fzf.rc.sh
+++ configs/fzf/fzf.rc.sh
@@ -3,23 +3,7 @@ eval "$(fzf --zsh)"
 
 
 export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
-
-# Use fd (https://github.com/sharkdp/fd) for listing path candidates.
-# - The first argument to the function ($1) is the base path to start traversal
-# - See the source code (completion.{bash,zsh}) for the details.
-_fzf_compgen_path() {
-  fd --hidden --exclude .git . "$1"
-}
-
-# Use fd to generate the list for directory completion
-_fzf_compgen_dir() {
-  fd --type=d --hidden --exclude .git . "$1"
-}
-
-# Use ? as the trigger sequence instead of the default **
-export FZF_COMPLETION_TRIGGER='?'
+export FZF_DEFAULT_OPTS="--layout=reverse --border" # Show input bar at the top
 
 SHOW_FILE_OR_DIR_PREVIEW="if [[ -d {} ]]; then 
                             tree {} | head -200;
@@ -33,14 +17,31 @@ OPENER="if [[ -d {} ]]; then
            nvim {};    # Open file with nvim 
        fi"
 
+# ctrl + t : find files
 export FZF_CTRL_T_OPTS="--preview '${SHOW_FILE_OR_DIR_PREVIEW}' \
                         --bind 'ctrl-e:execute:(${OPENER})'"
 #                        --bind 'ctrl-e:become:(${OPENER})' \
-#                        --bind 'ctrl-e:become:(${OPENER})' \
-#                       "
-#                       --tmux 100%,100% \
+#                       --tmux 100%,100% \"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
+# alt + c : find directory
 export FZF_ALT_C_OPTS="--preview 'tree | head -200'"
+export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
+
+# Use ? as the trigger sequence instead of the default **
+export FZF_COMPLETION_TRIGGER='?'
+
+# - The first argument to the function ($1) is the base path to start traversal
+# - See the source code (completion.{bash,zsh}) for the details.
+_fzf_compgen_path() {
+  fd --hidden --exclude .git . "$1"
+}
+
+# Use fd to generate the list for directory completion
+_fzf_compgen_dir() {
+  fd --type=d --hidden --exclude .git . "$1"
+}
+
 
 # Advanced customization of fzf options via _fzf_comprun function
 # - The first argument to the function is the name of the command.
