6973d0097c554dd864d1deb0664ed4a60c098e73
navin patidar <navin.patidar@gmail.com>
2026-02-04 21:06:06 -0800

    [tmux] add switch action to ts comamnd

--- configs/tmux/tmux.rc.sh
+++ configs/tmux/tmux.rc.sh
@@ -2,7 +2,8 @@ alias t="tmux"
 alias jk="tmux copy-mode"
 alias ta="tmux attach"
 
-LIST_WINDOWS="tmux list-panes -aF '#{session_name}:#{window_index} #{window_index} #{window_name}' |  grep -v \"${TMUX_PANE}\""
+LIST_WINDOWS="tmux list-panes -aF '#{session_name}:#{window_index}.#{pane_index} #{pane_current_command}'| grep -v \"${TMUX_PANE}\""
 PREVIEW_PANE="tmux capture-pane -p -e -t {1} | bat --color=always"
+SWITCH_PANE="enter:become:tmux switch-client -t {1}"
 
-alias ts="${LIST_WINDOWS} |  fzf --delimiter=' ' --preview=\"${PREVIEW_PANE}\" "
+alias ts="${LIST_WINDOWS} |  fzf --delimiter=' ' --preview=\"${PREVIEW_PANE}\" --bind=\"${SWITCH_PANE}\" "
