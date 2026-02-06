85b39b5fa7615f9e47f7874f961030c6f2540bfd
navin patidar <navin.patidar@gmail.com>
2026-02-05 20:03:05 -0800

    [nvim] add <leader>tt key to toggle teminal

--- configs/lazyVim/lua/plugins/toggle_term.lua
+++ configs/lazyVim/lua/plugins/toggle_term.lua
@@ -2,4 +2,28 @@ return {
   "akinsho/toggleterm.nvim",
   version = "*",
   opts = {},
+  config = function()
+    require("toggleterm").setup({
+      open_mapping = "<leader>tt", --set the normal mode keymap
+      direction = "float", -- open in floating window
+      float_opts = {
+        width = function()
+          return math.ceil(vim.o.columns * 0.6) -- 60% of total columns
+        end,
+        height = function()
+          return math.ceil(vim.o.lines * 0.8) -- 80% of total lines
+        end,
+        col = function()
+          --Move terminal to right : tolal columns minus terminal width
+          local width = math.ceil(vim.o.columns * 0.8)
+          return vim.o.columns - width
+        end,
+        border = "curved",
+      },
+      -- other settings:
+      start_in_insert = true,
+      persist_size = true,
+      winblend = 25,
+    })
+  end,
 }
