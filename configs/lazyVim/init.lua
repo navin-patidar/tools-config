ea683bc2429b7d8e34aab2f25ade199aeef96d99
navin patidar <navin.patidar@gmail.com>
2026-03-30 20:40:14 -0700

    nvim: restore the session automatically

--- configs/lazyVim/init.lua
+++ configs/lazyVim/init.lua
@@ -1,2 +1,21 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Detect stdin input so we don't restore session when piping
+vim.api.nvim_create_autocmd("StdinReadPre", {
+  group = vim.api.nvim_create_augroup("DetectStdin", { clear = true }),
+  callback = function()
+    vim.g.started_with_stdin = true
+  end,
+})
+
+-- Auto restore previous session when opening nvim without file argument
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = vim.api.nvim_create_augroup("RestoreSession", { clear = true }),
+  callback = function()
+    if vim.fn.argc() == 0 and not vim.g.started_with_stdin then
+      require("persistence").load({ last = true })
+    end
+  end,
+  nested = true,
+})
