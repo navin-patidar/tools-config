5c2a3948e1a097e16c4198521ba097f17cb9b7b5
navin patidar <navin.patidar@gmail.com>
2026-06-22 17:46:49 -0700

    fix kitty config file path

--- configs/kitty/kitty.rc.sh
+++ configs/kitty/kitty.rc.sh
@@ -1,7 +1,8 @@
 # Symlink kitty config into ~/.config so kitty picks it up
-KITTY_SRC="${TOOLS_CONFIG_DIR}/configs/kitty"
-KITTY_DST="${HOME}/.config/kitty"
+KITTY_SRC="${TOOLS_CONFIG_DIR}/configs/kitty/"
+KITTY_DST="${HOME}/.config/"
 if [[ -d "$KITTY_SRC" ]] && [[ ! -L "$KITTY_DST" || "$(readlink "$KITTY_DST")" != "$KITTY_SRC" ]]; then
   mkdir -p "${HOME}/.config"
   ln -sfn "$KITTY_SRC" "$KITTY_DST"
 fi
+
